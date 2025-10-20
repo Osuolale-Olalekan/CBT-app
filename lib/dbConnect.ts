164f0026a18780216f6451809ebf61e65f093209
Olalekan <osuolaleolalek7@gmail.com>
2025-10-20 00:23:14 -0700

    increaed db timing to 30 sec

--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -27,7 +27,11 @@ const dbConnect = async () => {
   }
 
   if (!cache.promise) {
-    const opts = { bufferCommands: false }; // 🔧 changed from true → false
+    const opts = { 
+      bufferCommands: false,
+      serverSelectionTimeoutMS: 30000, // Increase timeout
+      socketTimeoutMS: 45000,
+    };
     cache.promise = mongoose.connect(URI, opts).then((mongoose) => mongoose);
   }
 
@@ -42,6 +46,56 @@ const dbConnect = async () => {
 };
 
 export default dbConnect;
+
+
+
+
+
+
+// import "server-only";
+// import mongoose from "mongoose";
+
+// declare global {
+//   // eslint-disable-next-line @typescript-eslint/no-explicit-any
+//   var mongoose: any;
+// }
+
+// const URI = process.env.MONGODB_URI;
+
+// if (!URI) {
+//   throw new Error("Can't find mongodb URI");
+// }
+
+// let cache = global.mongoose;
+
+// if (!cache) {
+//   cache = global.mongoose = {
+//     conn: null,
+//     promise: null,
+//   };
+// }
+
+// const dbConnect = async () => {
+//   if (cache.conn) {
+//     return cache.conn;
+//   }
+
+//   if (!cache.promise) {
+//     const opts = { bufferCommands: false }; // 🔧 changed from true → false
+//     cache.promise = mongoose.connect(URI, opts).then((mongoose) => mongoose);
+//   }
+
+//   try {
+//     cache.conn = await cache.promise;
+//   } catch (error) {
+//     cache.promise = null;
+//     throw error;
+//   }
+
+//   return cache.conn;
+// };
+
+// export default dbConnect;
 
 
 
